28c3c675f7d0d79652955053c9dcaa39585d953e
cwcor <cwcor@DESKTOP-KLH00TH>
2016-09-03 16:19:41 -0500

    Add project files.

---
+++ BullySquasher/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BullySquasher.Startup))]
+namespace BullySquasher
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
